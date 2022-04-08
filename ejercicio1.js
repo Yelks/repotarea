eb48949edd7374fa4728c0a6aa1ea8da9239a47d
yelks <yelsin2335@gmail.com>
2022-04-08 17:28:12 -0600

    tarea

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+const kelvin = 0;
+// line to set kelvin like a constant
+let celsius = kelvin - 273 ;
+// another comment
+
+let fahrenheit = celsius* (9/5) + 32;
+//comentario
+var sol = Math.ceil(fahrenheit);
+// comentario
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+let myAge =  26;
+//
+let earlyYears = 2;
+//
+earlyYears = earlyYears * 10.5;
+var laterYears = myAge - 2;
+//
+laterYears *= 4;
+myAgeIndogYears= earlyYears + laterYears;
+//
+
+var myName = "YElSiN CRUZ AVIleZ".toLowerCase();
+//
+console.log(`My name is: ${myName}, I am ${myAge} years old in human years which is ${myAgeIndogYears} years old in dog years`);
